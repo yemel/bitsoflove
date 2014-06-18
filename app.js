ce407d824c29e6c5e54f07cd7c9cf9803dd9a072
Yemel Jardi <angel.jardi@gmail.com>
2014-06-18 10:19:06 -0300

    Twitter sign in

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var path = require('path');
 var passport = require('passport')
 var logfmt = require('logfmt');
 var request = require('request');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var TwitterStrategy = require('passport-twitter').Strategy;
 
@@ -33,12 +36,22 @@ passport.use(new TwitterStrategy({
 
 // APPLICATION
 var app = express();
+
+var cons = require('consolidate');
+app.engine('hbs', cons.handlebars);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.use(logfmt.requestLogger());
 app.use(express.static(__dirname + '/public'));
+
+app.use(cookieParser());
+app.use(session({ secret: process.env.SECRET }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+/* Auth end points */
 app.get('/auth/twitter',
   passport.authenticate('twitter'),
   function(req, res){
@@ -57,8 +70,11 @@ app.get('/logout', function(req, res){
 
 /* GET home page. */
 app.get('/', function(req, res) {
-  console.log(req.user);
-    res.send('mundo');
+  res.render('index', {user: req.user});
+});
+
+app.get('/love/h/bitcoin', function(req, res) {
+  res.render('wall', {topic: '#bitcoin', user: req.user});
 });
 
 var port = Number(process.env.PORT || 3000);
